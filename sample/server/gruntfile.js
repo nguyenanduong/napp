ab89927344bf48ea7452120e4f55b63f79cba11b
unknown <nguyenad@SEA-1201037809.ant.amazon.com>
2014-12-26 08:41:37 +0000

    Added require.config.js generation task to grunt file

--- sample/server/gruntfile.js
+++ sample/server/gruntfile.js
@@ -17,62 +17,94 @@ module.exports = function(grunt) {
           suites: grunt.file.expand(["test/**/*.js", "!test/intern.js"])
         }
       }
-    },
-    shell: {
-      jam: {
-        command: "jam install"
-      }
-    },
-    npm_modules: {
-
     }
   });
 
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("intern");
-  grunt.loadNpmTasks("grunt-shell");
 
   // Task(s).
   grunt.registerTask("install_npm_modules", "Setup npm package dependencies", function () {
     // loop through amd modules to find node dependencies
-    done = this.async();
+    var done = this.async();
     deps = [];
-    grunt.file.expand(["amd_modules/**/package.json"]).forEach(function (packageJsonPath) {
+    grunt.file.expand(["amd_modules/*/package.json"]).forEach(function (packageJsonPath) {
       var packageJson = grunt.file.readJSON(packageJsonPath);
-      console.log(packageJsonPath);
       for (dep in packageJson.dependencies || {}) {
-        console.log("  " + dep + "@\"" + packageJson.dependencies[dep] + "\"");
         deps.push(dep + "@" + packageJson.dependencies[dep]);
       }
     })
-    grunt.util.spawn({
-      cmd: "npm",
-      args: ["install"].concat(deps),
-      opts: {
-        stdio: "inherit"
+    var npm = require("npm");
+    npm.load(function (err, npm) {
+      if (err) {
+        grunt.log.error(err);
+        return;
       }
-    }, function (err) {
-      if (!err) {
-        console.log("Node modules installed");
-      } else {
-        console.log(err);
-      }
-      done();
+
+      npm.commands.install(deps, function (err) {
+        if (!err) {
+          console.log("Node dependencies from AMD packages installed");
+        } else {
+          console.log("Error: " + err);
+        }
+        done();
+      });
     });
   });
 
   grunt.registerTask("generate_require_config", "", function () {
-    grunt.file.expand(["amd_modules/**/package.json"]).forEach(function (packageJsonPath) {
+    var done = this.async();
+    var path = require("path");
+    var packages = [];
+    var mainPackage = {
+      name: grunt.config("pkg").name,
+      location: "."
+    };
+    grunt.file.expand(["amd_modules/*/package.json"]).forEach(function (packageJsonPath) {
       var packageJson = grunt.file.readJSON(packageJsonPath);
-      console.log(packageJsonPath);
-      for (dep in packageJson.dependencies || {}) {
-        console.log("  " + dep + "@\"" + packageJson.dependencies[dep] + "\"");
-        deps.push(dep + "@" + packageJson.dependencies[dep]);
+      packages.push({
+        name: packageJson.name,
+        location: path.dirname(packageJsonPath),
+        main: packageJson.main
+      })
+    });
+
+    var content = grunt.template.process(
+      "(function (define) {\n" +
+      "    define({\n" +
+      "        packages: [\n" +
+      "<% _.forEach(deps, function(dep) { %>" +
+      "            {\n" +
+      "                \"name\": \"<%- dep.name %>\",\n" +
+      "                \"location\": \"<%- dep.location %>\"" +
+      "<%     if (dep.main) { %>" +
+      ",\n                \"main\": \"<%- dep.main %>\"\n" +
+      "            },\n" +
+      "<%     } else {%>" +
+      "\n" +
+      "            },\n" +
+      "<%     }%>" +
+      "<% }); %>" +
+      "            {\n" +
+      "                \"name\": \"<%- main.name %>\",\n" +
+      "                \"location\": \".\"\n" +
+      "            }\n" +
+      "        ]\n" +
+      "    });\n" +
+      "})((typeof define === \"function\" && define.amd) ? define : function (exp) {\n" +
+      "    module.exports = exp;\n" +
+      "});\n",
+      {
+        data: { main: mainPackage, deps: packages }
       }
-    })
+    );
+
+    grunt.file.write("require.config.js", content);
+
+    console.log("require.config.js generated.");
   });
 
-  grunt.registerTask("setup", ["shell:jam", "install_npm_modules", "generate_require_config"]);
+  grunt.registerTask("setup", ["install_npm_modules", "generate_require_config"]);
   grunt.registerTask("test", ["intern"]);
   grunt.registerTask("default", ["setup", "jshint", "intern"]);
 };
